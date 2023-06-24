3a0bf2eb7a779ccc11da6c94bcbbeb2932b8ad07
Marina Barbosa <marinabspanenberg@gmail.com>
2023-06-24 10:25:40 -0300

    Foi adicionado o bloco que realiza a soma das distâncias das estrelas em pixels e escreve na tela, no meio da linha que liga elas, o valor encontrado. Ele está definido como sendo uma função.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+
 #biblioteca de serialização(obj->bytes) e de desserialização(bytes->obj)
 import pickle 
 from tkinter import simpledialog
@@ -14,10 +15,8 @@ pygame.display.set_icon(reduzirIcone)
 branco = (225,225,225)
 clock = pygame.time.Clock()
 running = True
-
 estrelas = {} #Dicionário
 posicoes = []
-
 posicaoX = []# posições separadas para ajudar a realização do ponto extra
 posicaoY = []
 
@@ -40,6 +39,7 @@ def instrucoes():
 def salvarProgresso():
     with open("DadosSalvos.pickle", "wb") as arquivo:
         pickle.dump((estrelas, posicoes), arquivo) #pickle.dump() = obj->bytes e grava ele em um arquivo
+    calcularDistancias()
 
 #Bloco para carregar as estrelas(=progresso) salvas e caso não tenha nenhum arquivo salvo(FileNotFoundError) é criado um arquivo 
 def carregarProgresso():
@@ -47,13 +47,26 @@ def carregarProgresso():
         with open("DadosSalvos.pickle", "rb") as arquivo:
             #pickle.load() = bytes->obj e lê o arquivo
             estrelas_salvas, posicoes_salvas = pickle.load(arquivo)
-            posicoes.clear()
             estrelas.clear()
+            posicoes.clear()
             estrelas.update(estrelas_salvas)
             posicoes.extend(posicoes_salvas)
+            
 
     except FileNotFoundError:
         salvarProgresso()
+    calcularDistancias()
+
+#Bloco para calcular a distância de cada estrela em pixels
+def calcularDistancias():
+    for i in range(len(posicoes) - 1):
+        x1, y1 = posicoes[i]
+        x2, y2 = posicoes[i + 1]
+        distancia = abs((x2 - x1) + (y2 - y1))
+        mediaX = (x1 + x2) // 2
+        mediaY = (y1 + y2) // 2
+        exibir = pygame.font.SysFont(None, 24).render(str(distancia), True, branco)
+        display.blit(exibir, (mediaX, mediaY))
 
 while running:
     for event in pygame.event.get():
@@ -73,30 +86,35 @@ while running:
             posicoes.append(posicao) 
             posicaoX.append(posicao[0])
             posicaoY.append(posicao[1])
-
+        
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_F10 or event.key == pygame.K_ESCAPE:
                salvarProgresso() 
 
             elif event.key == pygame.K_F11:
                 carregarProgresso()
+        
 
             #Bloco que exclui as estrelas(=progresso) salvas
             elif event.key == pygame.K_F12:
                 estrelas.clear() #Limpa o dicionário
                 posicoes.clear()
-                salvarProgresso() #salva o pickle em branco
-                
-
+                posicaoX.clear()
+                posicaoY.clear()
+                salvarProgresso() #salva 00 pickle em branco
+                calcularDistancias()
+        
     display.blit(fundo,(0,0))
+    calcularDistancias()
     instrucoes()
-
+    
     #Cria a mensagem e posiciona onde foi clicado        
     for pergunta, posicao in estrelas.items():
         mensagem_display = pygame.font.SysFont(None, 24).render(pergunta, True, branco)
         posicao_mensagem = (posicao[0]-10, posicao[1]+10) # ajusta a mensagem na tela
         display.blit(mensagem_display, posicao_mensagem)
         pygame.draw.circle(display, branco, posicao,4)
+        
     #Adiciona uma linha que liga uma estrela na outra
     if len(posicoes) > 1:
         pygame.draw.lines(display, branco, False, posicoes, 1)
